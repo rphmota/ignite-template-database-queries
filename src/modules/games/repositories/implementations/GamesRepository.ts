fd6b5e6553a33e2b517e6a8d9a9d980f451f204e
Raphael Mota <rphmota@gmail.com>
2022-04-28 15:27:39 -0300

    challenge done

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,22 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
+    return await this.repository
+      .createQueryBuilder('games')
+      .where('games.title ilike :name',{name:`%${param}%`})
+      .getMany()
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('Select count(*) from games'); 
+    // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    return await this.repository
+    .createQueryBuilder("game").where("game.id = :id", { id })
+    .relation(Game, "users").of(id).loadMany();
+     // Complete usando query builder
   }
 }
